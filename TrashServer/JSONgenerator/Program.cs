7229fe7bee6abb18c73bf99b7056cc526c66b074
Klaas Govaerts <govaerts.klaas@gmail.com>
2018-11-09 23:53:57 +0100

    Revert "Small changes"
    This reverts commit 337e6060234dad36306a14ec490c489136bc55fc.

---
+++ TrashServer/JSONgenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace JSONgenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrashCanLevel[] trashCans = new TrashCanLevel[1799];
+            for (int i = 0; i < 1799; i++)
+            {
+                trashCans[i]= new TrashCanLevel() { Level = i%100, ID = i };
+            }
+            
+            string json = JsonConvert.SerializeObject(trashCans);
+            Console.WriteLine(json);
+            File.WriteAllText(@"trash.json",json);
+            Console.ReadLine();
+        }
+    }
+}
